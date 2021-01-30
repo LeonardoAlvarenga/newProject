38dd6ebbd8ca9dd8e1ee7f488410ba7de349ab50
Leonardo Alvarenga <leonardo.mad@hotmail.com>
2021-01-30 03:02:23 -0300

    script.js - random message generator

---
+++ script.js
@@ -0,0 +1,15 @@
+const messages = [
+  "Hello!",
+  "Hey!",
+  "Hola!",
+  "Olá!",
+  "Opa!",
+  "Oi!",
+  "Hi!",
+  "Hue!",
+  "Jorge!",
+];
+
+const indexMessage = Math.floor(Math.random() * messages.length);
+
+console.log(messages[indexMessage]);
